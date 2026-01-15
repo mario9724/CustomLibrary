308f8deeea2b7401a394a5333ae80bf799a47597
Mamomem <mario9724@gmail.com>
2026-01-15 02:48:03 +0100

    Actualizar server.js

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
